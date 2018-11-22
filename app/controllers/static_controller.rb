d104a09ac8cd387547c3577debf8e752d29a5c1d
dsound-zz <demiansims@gmail.com>
2018-11-21 21:17:41 -0500

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,19 @@
+class StaticController < ApplicationController
+
+  def hello_world
+
+    render "hello_world"
+
+  end
+
+  # def new
+  # end
+  #
+  # def show
+  # end
+  #
+  # def create
+  # end
+
+
+end
